8d93f00058f2e5c16c49b311bf65c8a6f882b425
kavyansh <kavyanshgarg27@gmail.com>
2025-10-25 19:22:20 +0530

    admin login comp

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("style.css");
   eleventyConfig.addPassthroughCopy("script.js");
   eleventyConfig.addPassthroughCopy("images");
-  eleventyConfig.addPassthroughCopy("admin");
+  eleventyConfig.addPassthroughCopy("/admin");
 
  // This is the FIXED code
 eleventyConfig.addFilter("readableDate", dateObj => {
