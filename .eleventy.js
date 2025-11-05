8d53ea63aaab6c2a5aa1f21862525ce974d822ef
kavyansh <kavyanshgarg27@gmail.com>
2025-11-05 13:29:33 +0530

    drafting functionality

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,21 @@
 module.exports = function(eleventyConfig) {
+// Determine if we're in production environment (e.g., Netlify build)
+const isProduction = process.env.NODE_ENV === 'production';
 
-  // Remove the starting "/"
+// Create a custom collection for your posts that filters drafts
+  eleventyConfig.addCollection("posts", function(collectionApi) {
+
+    // Get all items that have the tag "post" (your blog posts)
+    const allPosts = collectionApi.getFilteredByTag("post"); 
+
+    // If we are building for production, filter out posts where 'draft' is true
+    if (isProduction) {
+      return allPosts.filter(item => !item.data.draft);
+    }
+
+    // If we are building locally (development), include all posts (drafts too)
+    return allPosts;
+  });
   eleventyConfig.addPassthroughCopy("style.css");
   eleventyConfig.addPassthroughCopy("script.js");
   eleventyConfig.addPassthroughCopy("images");
